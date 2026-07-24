538bd5a1902e654ca1c0762bc329e3412d3bcc04
jewel998 <jewel998@users.noreply.github.com>
2026-07-24 14:32:04 +0000

    deploy: ca56cacdbb2fd0e6b86125075338ad3a8c0f08f8

---
+++ portal/assets/skeleton-CEnkzBuV.js
@@ -0,0 +1,1 @@
+import{i as e}from"./vendor-react-Cw5kOvTj.js";import{r as t}from"./project-context-U7YuCFAy.js";var n=e();function r({className:e,...r}){return(0,n.jsx)(`div`,{"data-slot":`skeleton`,className:t(`animate-pulse rounded-md bg-muted`,e),...r})}export{r as t};
