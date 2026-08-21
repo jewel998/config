52dd9c89e89c1e2796f2b50a8dd3a530ee1a437e
jewel998 <jewel998@users.noreply.github.com>
2026-08-21 10:40:25 +0000

    deploy: 4a529db70bd169e37e78489be458b40899908aae

---
+++ assets/chunks/swimlanesDiagram-VK2B7HYN.8imdwcMx.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-A5DVABFB.BC9KFZkL.js";import{_ as a}from"../app.tqY2dnLh.js";import"./chunk-5VM5RSS4.DQPwVEXj.js";import"./chunk-XXDRQBXY.Z0dkeRou.js";import"./chunk-POPQ4Y6H.Dll_hyJD.js";import"./chunk-F27PBJKO.Cw_phdaM.js";import"./channel.B6UGvuBF.js";import"./framework.BxrwG5q5.js";import"./theme.AT__50hH.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
